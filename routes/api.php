74b9a0013ea8c2c69e6e0e79f5893ce01d316122
Jose Arjona <jose.jao71jao@hotmail.com>
2022-01-28 08:08:21 +0100

    usuarios

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,9 @@ use Illuminate\Support\Facades\Route;
 use Psr\Http\Message\ServerRequestInterface;
 use Tqdev\PhpCrudApi\Api;
 use Tqdev\PhpCrudApi\Config;
+use Illuminate\Validation\ValidationException;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,6 +37,27 @@ use Tqdev\PhpCrudApi\Config;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/tokens/create', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required'
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return response()->json([
+        'token_type' => 'Bearer',
+        'access_token' => $user->createToken('token_name')->plainTextToken // token name you can choose for your self or leave blank if you like to
+    ]);
+});
+
 
 Route::apiResource('centros', CentroController::class);
 
@@ -55,8 +79,6 @@ Route::apiResource('grupos', GrupoController::class);
 
 Route::apiResource('tutorizados', TutorizadoController::class);
 
-
-
 Route::apiResource('materias', MateriaController::class);
 
 Route::apiResource('periodosLectivos', PeriodoLectivoController::class);
@@ -66,6 +88,9 @@ Route::apiResource('materiasmatriculadas', MateriaMatriculadaController::class)
 
 ]);
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 Route::any('/{any}', function (ServerRequestInterface $request) {
     $config = new Config([
@@ -80,6 +105,4 @@ Route::any('/{any}', function (ServerRequestInterface $request) {
     return $response;
 })->where('any', '.*');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
